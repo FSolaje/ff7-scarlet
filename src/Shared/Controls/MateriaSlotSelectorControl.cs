449dbceec2362273ff0e831fa83c056e2c4ba67c
fsolaje <fsolaje@gmail.com>
2025-09-21 16:20:13 +0200

    docs(i18n): Translate remaining Spanish comments to English
    This commit translates the last remaining Spanish comments in MateriaSlotSelectorControl.cs to English, ensuring full localization of code comments.

--- src/Shared/Controls/MateriaSlotSelectorControl.cs
+++ src/Shared/Controls/MateriaSlotSelectorControl.cs
@@ -1,4 +1,4 @@
-using FF7Scarlet.KernelEditor;
+﻿using FF7Scarlet.KernelEditor;
 using Shojy.FF7.Elena.Equipment;
 using Shojy.FF7.Elena.Inventory;
 using Shojy.FF7.Elena.Materias;
@@ -355,7 +355,7 @@ namespace FF7Scarlet.Shared.Controls
                         InvokeDataChanged(this, EventArgs.Empty);
                     });
 
-            // TODO Sería conveniente convetir esto en una función aparte.
+            // TODO  It would be convenient to convert this into a separate function.     
             if (SlotSelectorType == SlotSelectorType.Slots)
             {
                 slotsProcessedSuccessfully
@@ -501,7 +501,7 @@ namespace FF7Scarlet.Shared.Controls
                 bool isLeftSlotDoubleLinked = GetEffectiveIsLeftSlotDoubleLinked(leftSlotIndex);
                 bool leftSlotHasChangeToRightLinked = GetLeftSlotHasChangeToRightLinked(leftSlotIndex, leftSlotValue);
 
-                // Comprueba el estado de los link del slot actual.
+                // Checks the current link status of the slot.
                 bool isUnlinkedFromRightSlot = IsUnlinkedFromRight(slotIndex, rightSlotIndex, rightSlotValue);
                 bool isUnlinkedFromLeftSlot = IsUnlinkedFromLeft(slotIndex, leftSlotIndex, leftSlotValue, isLeftSlotDoubleLinked, leftSlotHasChangeToRightLinked, updateDirection);
 
@@ -539,7 +539,6 @@ namespace FF7Scarlet.Shared.Controls
         /// <summary>
         /// Determines if the left slot has just changed from a left-linked to a right-linked state.
         /// This is a specific edge case that breaks the link with the current slot.
-        /// Note that a Double-linked slot is also considered Right-linked for this check.
         /// </summary>
         /// <param name="leftSlotIndex">The index of the left slot.</param>
         /// <param name="leftSlotValue">The current value of the left slot.</param>
